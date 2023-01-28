98e6cb3894fbaf12bfafa9e28863aab5a3727320
Bruno Vasconcelos <brunojasgv@gmail.com>
2023-01-28 11:51:34 +0000

    Initial Commit

---
+++ BrowsrApp/BrowsrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowsrAppApp.swift
+//  BrowsrApp
+//
+//  Created by BRUNO VASCONCELOS on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrowsrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
